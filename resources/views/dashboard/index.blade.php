2b2abe2beb35e03855817d678966f35ef47278f5
FrontendMaybeBackend <krzysztof231201@interia.pl>
2023-04-29 17:08:35 +0200

    make dashboard with displays value from users/clients and projects, add some layout

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,23 +1,35 @@
 @php use Illuminate\Support\Facades\DB; @endphp
 @extends('layouts.app')
 
+<head>
+    <link rel="stylesheet" href="{{url('dashboard.css')}}">
+</head>
 @section('content')
     <h1>Dashboard</h1>
-    <div class="container  p-5">
-        <div class="row">
-            <div class="col text-center p-2 border-dark border">
-                <p>Users:</p>
-                {{ $users = DB::table('users')->count() }}
+    <div class="row justify-content-between p-5 ">
+        <div class="col-4 ">
+            <div class="col text-center p-2  border-start border-success border-5 bg-white rounded-2 ">
+                <img class="" src="{{url('/images/user3.png')}}" alt="usersicon">
+                <p class="lead">Users:</p>
+                <p class="lead"> {{ $users = DB::table('users')->count() }} </p>
+                <button class="btn btn-info">Check</button>
             </div>
-            <div class="col  text-center p-2 border-dark border">
-                <p>Clients:</p>
-                {{ $clients = DB::table('clients')->count() }}
+        </div>
+        <div class="col-4  ">
+            <div class="col  text-center p-2  border-start border-success border-5 bg-white rounded-2">
+                <img class="" src="{{url('/images/clients.jpg')}}" alt="usersicon">
+                <p class="lead" >Clients:</p>
+                <p class="lead"> {{ $clients = DB::table('clients')->count() }}</p>
+                <button class="btn btn-info">Check</button>
             </div>
-            <div class="col text-center p-2 border-dark border ">
-                <p>Projects:</p>
-                {{ $projects = DB::table('projects')->count() }}
+        </div>
+        <div class="col-4 ">
+            <div class="col text-center p-2  border-start border-success border-5 bg-white  rounded-2">
+                <img class="" src="{{url('/images/project.png')}}" alt="projecticon">
+                <p class="lead" >Projects:</p>
+                <p class="lead"> {{ $projects = DB::table('projects')->count() }}</p>
+                <button class="btn btn-info">Check</button>
             </div>
         </div>
     </div>
-
 @endsection
